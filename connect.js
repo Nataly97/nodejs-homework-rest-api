68d9ba496a6a278a85f66aa05dc27d343955380b
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-10-30 19:08:55 -0500

    Conexión

---
+++ connect.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+
+//Conexión a DB
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.CONNECTION_MONGODB)
+        .then(() => console.log('Database connection successful'))
+    } catch (error) {
+        console.log(`Error de conexión: ${error}`);
+        process.exit(1);
+    }
+   
+}
+
+module.exports ={
+    connect,
+}
