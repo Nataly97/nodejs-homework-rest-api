df890b2a66d15e8f4be242c267debc1f535b8346
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-11-23 16:30:24 -0500

    Ajustes Schema PATCH User

--- models/userSchemaValidations.js
+++ models/userSchemaValidations.js
@@ -3,13 +3,14 @@ const userSchemaValidations = (body) => {
     const Schema = joi.object({
         email: joi.string().email().required(),
         password: joi.string().required(),
+        subscription: joi.string().valid("starter", "pro", "business"),
     });
     return Schema.validate(body);
 }
 
 const patchUserSchemaValidations = (body) => {
     const Schema = joi.object({
-        subscription: joi.string().required(),
+        subscription: joi.string().valid("starter", "pro", "business"),
     });
     return Schema.validate(body);
 }
